46b8a780306eae5979ece6aa13fc799151ec90be
Aliaksei Demukh <aliaksei_demukh@epam.com>
2016-04-14 16:01:24 +0300

    added initial files

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
 
-// app.set('port', (process.env.PORT || 5000));
-app.use(express.static('public'));
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/todo', function (req, res) {
+  res.send('Index.js!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
