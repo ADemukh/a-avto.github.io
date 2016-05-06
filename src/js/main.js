2fef410ad492ce0732f2d4cfff199c07e4e35c77
Aliaksei Demukh <aliaksei_demukh@epam.com>
2016-05-06 16:25:04 +0300

    added html:build, js:build gulp tasks

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
 /*
  * Third party
  */
+ 
 //= ../../bower_components/jquery/dist/jquery.min.js
-//= ../../bower_components/bootstrap/dist/js/bootstrap.min.js.js
+//= ../../bower_components/bootstrap/dist/js/bootstrap.min.js
 //= ../../bower_components/angular/angular.min.js
 
 /*
