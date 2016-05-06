8643851c085195142c0cf0ac4f4e6c433de4070e
Aliaksei Demukh <aliaksei_demukh@epam.com>
2016-05-06 16:10:32 +0300

    1. Included:  - gulp js  - bower js 2. Renamed public folder to src

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    prefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rigger = require('gulp-rigger'),
+    cssmin = require('gulp-minify-css'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    rimraf = require('rimraf'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload;
---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+/*
+ * Third party
+ */
+//= ../../bower_components/jquery/dist/jquery.min.js
+//= ../../bower_components/bootstrap/dist/js/bootstrap.min.js.js
+//= ../../bower_components/angular/angular.min.js
+
+/*
+ * Custom
+ */
+//= partials/app.js
