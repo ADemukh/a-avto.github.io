382e8b2feb28466309881b3075fadb898e6a6975
Aleksey Demukh <aleksey.demukh@gmail.com>
2016-08-28 15:20:07 +0300

    update node version

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/.build'));
+app.use(express.static(__dirname + '/.tmp'));
 
 app.get('/todo', function (req, res) {
   res.send('Index.js!');
