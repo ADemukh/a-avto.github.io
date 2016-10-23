42d58a600a53e02aa954b02a5477937854fc3725
Artem <Punko>
2016-10-23 23:44:36 +0300

    e

---
+++ src/js/services/shops/shops.service.js
@@ -0,0 +1,19 @@
+// (function ShopsServiceInit() {
+//     'use strict';
+
+//     angular.module('services')
+//         .factory('shops', ShopsService);
+
+//     function ShopsService(){
+//         var shops = {
+//             Auto: {55.831903, 37.411961},
+//             Auto1: {55.763338,37.565466},
+//             Auto2: {55.763338,37.565466}};
+
+//         function getCoordinats
+
+
+//     };
+//         return{};
+//     }
+// })();
