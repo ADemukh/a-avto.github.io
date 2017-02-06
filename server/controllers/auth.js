d4197ff8b49dc5894ea3a53e6252604e6f67d4fe
Demuh Sergey <demuh.sergey10@gmail.com>
2017-02-07 00:16:26 +0300

    I sent email

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,9 +1,10 @@
 /*eslint strict:0  */
-var auth, passport, User, Shop;
+var auth, passport, User, Shop, nodemailer, transporter, mailOptions;
 
 User = require('../models/user');
 Shop = require('../models/shop');
 passport = require('passport');
+nodemailer = require('nodemailer');
 
 auth = {
     isAuthenticated: function isAuthenticated(req, res, next) {
@@ -31,21 +32,46 @@ auth = {
         };
     },
     recoverPassword: function recoverPassword(email) {
-       return User.findOne({
-            email: email
-        }).exec()
-        .then(function userFound(user) {
-            return user ? user : Shop.findOne({
+        return User.findOne({
                 email: email
-            }).exec();
-        })
-        .then(function userFound(user) {
-            if (user) {
-                //senд email
-                return true;
-            }
-            return false;
-        });
+            }).exec()
+            .then(function userFound(user) {
+                return user ? user : Shop.findOne({
+                    email: email
+                }).exec();
+            })
+            .then(function userFound(user) {
+                if (user) {
+                    transporter = nodemailer.createTransport({
+                        service: 'gmail',
+                        auth: {
+                            user: 'aleksey.demukh',
+                            pass: 'aleksey10051990'
+                        }
+                    });
+                    mailOptions = {
+                        from: 'demuh.sergey10@gmail.com',
+                        to: user.email,
+                        subject: 'Востоновление пороля',
+                        html: 'Ваш потеряный пороль' + user.password
+                    };
+                    return transporter.sendMail(mailOptions);
+                }
+            })
+            .then(function sent(info) {
+                if (info && info.accepted && info.accepted.length) {
+                    console.log('Письмо успешно отправлено.');
+                    return true;
+                }
+                console.log('письмо небыло оправлено');
+                return false;
+            })
+            .catch(function failed(error) {
+                if (error) {
+                    console.log('Ошибка отправки: ' + error);
+                    return false;
+                }
+            });
     }
 };
 module.exports = auth;
