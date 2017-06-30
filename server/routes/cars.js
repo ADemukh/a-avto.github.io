136b5706e408ef68417d4635d4117971d89f8730
Demuh Sergey <demuh.sergey10@gmail.com>
2017-06-30 16:48:45 +0300

    I did update cars, delete cars

--- server/routes/cars.js
+++ server/routes/cars.js
@@ -23,17 +23,97 @@ router.get('/add', function addCarGet(req, res) {
   res.render('addCar', {});
 });
 
+// /cars/marks
+router.get('/marks', function getAllCars(req, res) {
+  carController.getAllMarks()
+    .then(function gotMarks(marks) {
+      res.render('cars/marks/allMarks', {
+        marks: marks
+      });
+    });
+});
+router.post('/marks/delete', function deleteCarMarks(req, res) {
+  carController.deleteCars(req.body.mark);
+  carController.getAllMarks()
+    .then(function gotMarks(marks) {
+      res.render('cars/marks/allMarks', {
+        marks: marks
+      });
+    });
+});
+
+router.get('/marks/:mark', function getAllModels(req, res) {
+  carController.getMark(req.params.mark)
+    .then(function gotMark(mark) {
+      res.render('cars/marks/mark', {
+        mark: mark
+      });
+    });
+});
+
+router.post('/marks/:mark/edit', function getAllModels(req, res) {
+  carController.updateMark({
+    newMark: req.body.mark,
+    oldMark: req.body.model
+  });
+  carController.getAllMarks()
+    .then(function gotMarks(marks) {
+      res.render('cars/marks/allMarks', {
+        marks: marks
+      });
+    });
+});
+
+router.get('/marks/:mark/:model', function getModel(req, res) {
+  carController.getModel(req.params)
+    .then(function gotModel(model) {
+      res.render('cars/models/editModel', {
+        model: model
+      });
+    });
+});
+
+router.post('/marks/:mark/:model/edit', function updateCar(req, res) {
+  carController.update({
+    mark: req.body.mark,
+    model: req.body.model,
+    from: req.body.from,
+    end: req.body.end
+  });
+  carController.getMark(req.params.mark)
+    .then(function gotMark(mark) {
+      res.render('cars/marks/mark', {
+        mark: mark
+      });
+    });
+});
+router.post('/marks/:mark/:model/delete', function deleteCar(req, res) {
+  carController.deleteCar(req.body.id);
+  carController.getAllMarks()
+    .then(function gotMarks(marks) {
+      res.render('cars/marks/allMarks', {
+        marks: marks
+      });
+    });
+});
+
+router.post('/marks/:mark/:model/add', function updateCar(req, res) {
+  carController.updateModel(req.body);
+});
+
 router.post('/add', function addCarPost(req, res) {
   carController.add({
     mark: req.body.mark,
     model: req.body.model,
     from: req.body.from,
     end: req.body.end
-  })
-  .then(function success(cars) {
-    console.log('finished.');
-    res.send('added.');
   });
+  carController.getAllMarks()
+    .then(function gotMarks(marks) {
+      res.render('cars/marks/allMarks', {
+        marks: marks
+      });
+    });
 });
 
 module.exports = router;
