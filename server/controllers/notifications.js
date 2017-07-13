ce79a55bab6d815194b489f2477c75448ec06f7c
Artem Punko <Artem Punko>
2017-07-13 16:56:07 +0300

    feat(addNotifications)

--- server/controllers/notifications.js
+++ server/controllers/notifications.js
@@ -2,9 +2,67 @@ var Notifications, notificationsInitialCollection;
 
 Notifications = require('../models/notifications');
 
+function saveNotification(notification) {
+	var notificationModel;
+
+	notificationModel = new Notifications({
+		name: notification.name,
+		type: notification.type,
+		forClient: notification.forClient || false,
+		forShop: notification.forShop || false
+	});
+
+	return notificationModel.save(function save(err) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(notification.type + ' is loaded.');
+		}
+	});
+}
 
 module.exports = {
+	addNotification: function addNewNotification(notification) {
+		return saveNotification(notification);
+	},
 	getNotifications: function getNotifications(filter) {
 		return Notifications.find(filter).exec();
+	},
+	deleteNotification: function deleteNotification(type) {
+		return Notifications.findOne({
+			type: type
+		}).exec()
+			.then(function foundNotification(notif) {
+				return notif.remove(function success(err) {
+					if (err) {
+						console.log(err);
+					} else {
+						console.log('DELETE removing ID: ' + notif.id);
+					}
+				});
+			});
+	},
+	updateNotification: function updateModel(notification) {
+		return Notifications.findById({
+			_id: notification.id
+		}).exec()
+			.then(function foundCar(notif) {
+				notif.name = notification.name;
+				notif.type = notification.type;
+				notif.forClient = notification.forClient;
+				notif.forShop = notification.forShop;
+
+				return notif.save()
+					.then(function success(savedNotification) {
+						console.log(savedNotification.type + ' is loaded.');
+					}, function failure(err) {
+						console.log(err);
+					});
+			});
+	},
+	getNotification: function getNotification(type) {
+		return Notifications.findOne({
+			type: type
+		}).exec();
 	}
 };
--- server/routes/notifications.js
+++ server/routes/notifications.js
@@ -11,4 +11,58 @@ router.get('/getNotifications', function getNotifications(req, res) {
     });
 });
 
+router.get('/notifications', function getAllCars(req, res) {
+  notificationsController.getNotifications({})
+    .then(function gotNotifications(notifications) {
+      res.render('notifications/allNotifications', {
+        notifications: notifications
+      });
+    });
+});
+
+router.get('/notifications/add', function addNotificationGet(req, res) {
+  res.render('notifications/addNotification', {});
+});
+
+router.post('/notifications/add', function addNotificationPost(req, res) {
+  notificationsController.addNotification({
+    name: req.body.name,
+    type: req.body.type,
+    forClient: req.body.forClient,
+    forShop: req.body.forShop
+  })
+    .then(function redirect() {
+      res.redirect('/notifications/notifications');
+    });
+});
+
+router.post('/notifications/delete', function deleteNotification(req, res) {
+  notificationsController.deleteNotification(req.body.type)
+    .then(function redirect() {
+      res.redirect('/notifications/notifications');
+    });
+});
+
+router.get('/notifications/:type', function getNotification(req, res) {
+  notificationsController.getNotification(req.params.type)
+    .then(function gotNotification(notification) {
+      res.render('notifications/editNotification', {
+        notification: notification
+      });
+    });
+});
+
+router.post('/notifications/:type/edit', function updateNotification(req, res) {
+  notificationsController.updateNotification({
+    id: req.body.id,
+    name: req.body.name,
+    type: req.body.type,
+    forClient: req.body.forClient,
+    forShop: req.body.forShop
+  })
+    .then(function redirect() {
+      res.redirect('/notifications/notifications');
+    });
+});
+
 module.exports = router;
