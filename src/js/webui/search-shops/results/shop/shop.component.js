e4a62e5e3537fcd371c9e49aaca2e866d481085e
Aliaksei Demukh <aliaksei_demukh@epam.com>
2017-08-23 18:34:43 +0300

    refactor(shop.component): replace heavy bootstrap component by lightweight elements

--- src/js/webui/search-shops/results/shop/shop.component.js
+++ src/js/webui/search-shops/results/shop/shop.component.js
@@ -6,11 +6,12 @@
             controller: 'controllers.searchshopsresultshop',
             templateUrl: 'webui/search-shops/results/shop/shop.tmpl.html',
             bindings: {
-                shop: '<'
+                shop: '<',
+                index: '<'
             }
         })
         .controller('controllers.searchshopsresultshop', function SearchShopsResultsShopController() {
-            var vm, DAY_TRANSLATIONS;
+            var DAY_TRANSLATIONS, vm;
 
             DAY_TRANSLATIONS = {
                 monday: 'MONDAY',
@@ -23,15 +24,23 @@
             };
 
             vm = this;
+            this.$onInit = function onInit() {
+                vm.tabIds = {
+                    home: 'HomeTab' + vm.index,
+                    cities: 'CitiesTab' + vm.index,
+                    spareTypes: 'SpareTypesTab' + vm.index,
+                    cars: 'CarsTab' + vm.index,
+                    workingHours: 'WorkingHoursTab' + vm.index
+                };
+                vm.visibleTab = 'home';
+                vm.getDayTranslationKey = function getDayTranslationKey(day) {
+                    return DAY_TRANSLATIONS[day];
+                };
+            };
             this.$onChanges = function onChanges(changes) {
                 if (changes.shop) {
                     this.shop = angular.copy(this.shop);
-                    this.shop.readonly = true;
                 }
-            };
-
-            vm.getDayTranslationKey = function getDayTranslationKey(day) {
-                return DAY_TRANSLATIONS[day];
             };
         });
 })();
