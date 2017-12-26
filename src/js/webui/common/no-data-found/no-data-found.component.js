5a9725b16a1d73ff54fe833526d5df1070276573
Naooki <sheynikovea@gmail.com>
2017-12-26 13:45:24 +0300

    feat(search-shops/search-orders/profile-orders): Shows no-data-template when no results found.

---
+++ src/js/webui/common/no-data-found/no-data-found.component.js
@@ -0,0 +1,8 @@
+(function NoDataFoundInit() {
+    'use strict';
+
+    angular.module(WEBUI_MODULE_NAME)
+        .component('qNoDataFound', {
+            templateUrl: 'webui/common/no-data-found/no-data-found.tmpl.html'
+        });
+})();
