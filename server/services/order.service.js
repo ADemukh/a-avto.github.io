a0ae76f760f7f538b8429dcf6394e59645c544b9
Aliaksei Demukh <aliaksei_demukh@epam.com>
2018-03-21 16:40:37 +0300

    feat(order): submit order

--- server/services/order.service.js
+++ server/services/order.service.js
@@ -1,128 +1,139 @@
-// const mongoose = require('mongoose');
-// const Order = require('../models/order');
-// const OrderShopDialog = require('../models/orderShopDialog');
-// const Message = require('../models/message');
-// const profileClientController = require('./profileClient');
+const Order = require('../models/order');
+const OrderShopDialog = require('../models/orderShopDialog');
+const Message = require('../models/message');
+const mongoose = require('mongoose');
+const shopService = require('./shop.service');
 
-// function submitClientOrder(orderInfo) {
-//     const car = getOrderCar(orderInfo);
-//     const orderId = new mongoose.Types.ObjectId();
+function getOrderCar(orderInfo) {
+    const car = orderInfo.car.selected._id === orderInfo.car.newCar._id ?
+        orderInfo.car.newCar :
+        orderInfo.car.selected;
+    delete car._id;
 
-//     const orderShopDialogs = getOrderDialogs(orderInfo, orderId);
+    return car;
+}
 
-//     orderModel = new Order({
-//         _id: orderId,
-//         title: orderInfo.details.title,
-//         description: orderInfo.details.description,
-//         spare: {
-//             isNewDetail: orderInfo.details.spare.isNewDetail,
-//             isUsedDetail: orderInfo.details.spare.isUsedDetail,
-//         },
-//         spareType: orderInfo.details.spareType,
-//         resolutionDate: orderInfo.details.resolutionDate,
-//         client: {
-//             user: orderInfo.client,
-//             car,
-//             contacts: orderInfo.contacts,
-//         },
-//         wantedList: orderInfo.wantedList,
-//     });
+function getInitialOrderMessage(orderInfo) {
+    return new Message({
+        author: orderInfo.client,
+        content: 'First message',
+    });
+}
 
-//     dialogs.forEach((dialog) => {
-//         orderModel.dialogs.push(dialog);
-//     });
+function getAvailableShops(orderInfo) {
+    if (orderInfo.shops.length) {
+        return Promise.resolve(orderInfo.shops);
+    }
+    return shopService.getAvailableShopsByOrder(orderInfo);
+}
 
-//     return orderModel.save()
-//         .then(() => OrderShopDialog.insertMany(dialogs))
-//         .then(() => {
-//             if (car.needSave) {
-//                 return profileClientController.addNewCar(orderInfo.contacts.email, orderInfo.car.newCar);
-//             }
-//         })
-//         .catch((err) => {
+function getOrderDialogs(orderInfo) {
+    const initialMessage = getInitialOrderMessage(orderInfo);
+    return getAvailableShops(orderInfo)
+        .then(shops =>
+            shops.map(shop => new OrderShopDialog({
+                _id: new mongoose.Types.ObjectId(),
+                order: orderInfo._id,
+                shop,
+                messages: [initialMessage],
+            })));
+}
 
-//         });
-// }
 
-// function getOrderCar(orderInfo) {
-//     const car = orderInfo.car.selected._id === orderInfo.car.newCar._id ?
-//         orderInfo.car.newCar :
-//         orderInfo.car.selected;
-//     delete car._id;
+function submitClientOrder(orderInfo) {
+    orderInfo._id = new mongoose.Types.ObjectId();
 
-//     return car;
-// }
+    return getOrderDialogs(orderInfo)
+        .then((dialogs) => {
+            const car = getOrderCar(orderInfo);
 
-// function getOrderDialogs(orderInfo, orderId) {
-//     return orderInfo.shops.map(shop => new OrderShopDialog({
-//         _id: new mongoose.Types.ObjectId(),
-//         order: orderId,
-//         shop,
-//         messages: [getFirstMessage(orderInfo)],
-//     }));
-// }
+            const order = new Order({
+                _id: orderInfo._id,
+                title: orderInfo.details.title,
+                description: orderInfo.details.description,
+                spare: {
+                    isNewDetail: orderInfo.details.spare.isNewDetail,
+                    isUsedDetail: orderInfo.details.spare.isUsedDetail,
+                },
+                spareType: orderInfo.details.spareType,
+                resolutionDate: orderInfo.details.resolutionDate,
+                client: {
+                    user: orderInfo.client,
+                    car,
+                    contacts: orderInfo.contacts,
+                },
+                wantedList: orderInfo.wantedList,
+            });
 
-// function getFirstOrderMessage(orderInfo) {
-//     return new Message({
-//         author: orderInfo.client,
-//         content: 'First message',
-//     });
-// }
+            dialogs.forEach((dialog) => {
+                order.dialogs.push(dialog);
+            });
 
-// function getOrders(filter) {
-//     let orderFilter;
+            return order.save()
+                .then(() => OrderShopDialog.insertMany(dialogs))
+                .then(() => {
+                    // return car.needSave ?
+                    //     profileClientController.addNewCar(orderInfo.contacts.email, orderInfo.car.newCar) :
+                    //     car;
+                    return car;
+                });
+        });
+}
 
-//     orderFilter = {};
-//     if (filter.orderCity) {
-//         orderFilter['client.contacts.city'] = {
-//             $in: [filter.orderCity],
-//         };
-//     }
-//     if (filter.carMark) {
-//         orderFilter['client.car.mark'] = {
-//             $in: [filter.carMark],
-//         };
-//     }
-//     if (filter.spareType) {
-//         orderFilter.spareType = {
-//             $in: [filter.spareType],
-//         };
-//     }
+function getOrders(filter) {
+    let orderFilter;
 
-//     if (filter.maxResolutionDate) {
-//         orderFilter.resolutionDate = {
-//             $lte: filter.maxResolutionDate,
-//         };
-//     }
+    orderFilter = {};
+    if (filter.orderCity) {
+        orderFilter['client.contacts.city'] = {
+            $in: [filter.orderCity],
+        };
+    }
+    if (filter.carMark) {
+        orderFilter['client.car.mark'] = {
+            $in: [filter.carMark],
+        };
+    }
+    if (filter.spareType) {
+        orderFilter.spareType = {
+            $in: [filter.spareType],
+        };
+    }
 
-//     if (filter.newDetail && !filter.usedDetail) {
-//         orderFilter['spare.isNewDetail'] = true;
-//     }
-//     if (filter.usedDetail && !filter.newDetail) {
-//         orderFilter['spare.isUsedDetail'] = true;
-//     }
+    if (filter.maxResolutionDate) {
+        orderFilter.resolutionDate = {
+            $lte: filter.maxResolutionDate,
+        };
+    }
 
-//     // if (filter.worksNow) {
-//     // 	currentDay = moment().format('dddd').toLowerCase();
-//     // 	currentTime = moment().format('HH:mm');
-//     // 	orderFilter['schedule.' + currentDay + '.active'] = true;
-//     // 	orderFilter['schedule.' + currentDay + '.from'] = { $lte: currentTime };
-//     // 	orderFilter['schedule.' + currentDay + '.to'] = { $gte: currentTime };
-//     // }
-//     // if (filter.worksOnWeekend) {
-//     // 	orderFilter.$or = [
-//     // 		{ 'schedule.saturday.active': true },
-//     // 		{ 'schedule.sunday.active': true }
-//     // 	];
-//     // }
-//     return Order.find(orderFilter).exec();
-// }
+    if (filter.newDetail && !filter.usedDetail) {
+        orderFilter['spare.isNewDetail'] = true;
+    }
+    if (filter.usedDetail && !filter.newDetail) {
+        orderFilter['spare.isUsedDetail'] = true;
+    }
 
-// module.exports = {
-//     submitClientOrder,
-//     getClientOrders,
-//     getClientOrderShops,
-//     getClientOrderShopMessages,
-//     getShopOrders,
-//     getShopOrderMessages,
-// };
+    // if (filter.worksNow) {
+    // 	currentDay = moment().format('dddd').toLowerCase();
+    // 	currentTime = moment().format('HH:mm');
+    // 	orderFilter['schedule.' + currentDay + '.active'] = true;
+    // 	orderFilter['schedule.' + currentDay + '.from'] = { $lte: currentTime };
+    // 	orderFilter['schedule.' + currentDay + '.to'] = { $gte: currentTime };
+    // }
+    // if (filter.worksOnWeekend) {
+    // 	orderFilter.$or = [
+    // 		{ 'schedule.saturday.active': true },
+    // 		{ 'schedule.sunday.active': true }
+    // 	];
+    // }
+    return Order.find(orderFilter).exec();
+}
+
+module.exports = {
+    submitClientOrder,
+    getClientOrders,
+    getClientOrderShops,
+    getClientOrderShopMessages,
+    getShopOrders,
+    getShopOrderMessages,
+};
